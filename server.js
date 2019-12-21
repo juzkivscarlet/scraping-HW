4ec2cc4e597965b6d268b1c09284eb6ee93c3eb6
Matt Juskiw <juskiwmatt@gmail.com>
2019-12-21 13:46:27 -0500

    Fixed port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 require('dotenv');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
